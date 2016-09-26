f61be55cacc9ef8c8e3ea0bf40fb744c380f5a58
Kristjan Gannon <gannonk08@gmail.com>
2016-09-26 13:57:32 -0600

    getting recording url bpm function probably not working

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -5,14 +5,18 @@ const beatDetector = require('../controllers/bpm-detector');
 
 router.get('/', function (req, res, next) {
   const renderObject = {};
-  const detectBPM = beatDetector.detectBPM;
-  const filePath ="../test_audio/amclassical_beethoven_fur_elise.aac"
   renderObject.title = 'Welcome to Moody';
-  var detectBPMVal = detectBPM(filePath)
-  setTimeout(() => {
-    renderObject.bpm = detectBPMVal
-    res.render('index', renderObject);
-  }, 500)
+  res.render('index', renderObject);
   });
+
+  router.post('/getBPM', function (req, res, next) {
+    const detectBPM = beatDetector.detectBPM;
+    // const filePath = req.body.recordingAddress;
+    const filePath = "../test_audio/BeethovensSymphonyNo.9scherzo_vbr.mp3";
+    var detectBPMVal = detectBPM(filePath)
+    setTimeout(() => {
+      res.json(detectBPMVal);
+    }, 500)
+    });
 
 module.exports = router;
