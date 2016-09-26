5156b2b16a0668c78fac0ad5ef6fece44efeb91a
Kristjan Gannon <gannonk08@gmail.com>
2016-09-25 20:47:25 -0600

    add header footer, bpm package -fix merge

---
+++ src/server/db/migrations/20160923142953_users.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', (t) => {
+      t.increments();
+      t.string('userName').notNullable();
+      t.string('email').notNullable();
+      t.string('song_played');
+      t.timestamp('created');
+      t.string('song_img');
+    });
+
+};
+
+exports.down = function(knex, Promise) {
+
+};
