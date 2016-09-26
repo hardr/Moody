72fff88ff20575bfc4d49e602b4eb6c0d018c518
Derek Styer <dmstyer@gmail.com>
2016-09-26 17:13:48 -0600

    routes

--- src/server/routes/login.js
+++ src/server/routes/login.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const indexController = require('../controllers/index');
+const knex = require('knex');
 let bcrypt = require('bcryptjs');
+
 
 router.get('/', function (req, res, next) {
   const renderObject = {};
@@ -9,37 +11,11 @@ router.get('/', function (req, res, next) {
     res.render('login', renderObject);
   });
 
-router.post('/login', (req, res, next) => {
-  let userEmail = req.body.email;
-  let password = req.body.password;
-  let msg;
-  if (!userEmail || !password) {
-    let results = {};
-    results.message = 'Must enter both username and password';
-    res.render('users/login', results);
-  } else {
-    knex('users')
-    .then((users) => {
-      let user = users.filter((user) => user.email === userEmail)[0];
-      if (!user) {
-        let results = {};
-        results.message = 'Incorrect username or password.';
-        res.render('users/login', results);
-      } else {
-        if (bcrypt.compareSync(password, user.password)) {
-          req.session.user = user;
-          res.redirect('/index');
-        } else {
-          let results = {};
-          results.message = 'Incorrect username or password.';
-          res.render('login', results);
-        }
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  }
-});
+// router.post('/', (req, res, next) => {
+//     knex('users').insert(
+//       first_name,
+//       last_name
+//
+//     )}
 
 module.exports = router;
