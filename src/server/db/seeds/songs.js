4bfb393d5d6c683e3483cd1dd63908bcd735e581
Derek Styer <dmstyer@gmail.com>
2016-09-27 13:58:02 -0600

    removed stop button

--- src/server/db/seeds/songs.js
+++ src/server/db/seeds/songs.js
@@ -1,13 +1,16 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('songs').del()
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
+        knex('songs').insert
+        ({ song_name: 'Hey Jude',
+           artist_name: 'Beatles',
+           user_id:3,
+           score:9
+         })
       ]);
     });
 };
