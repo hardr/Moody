d54cd2278244f05a9616d512ddaa35063ac08e11
Kristjan Gannon <gannonk08@gmail.com>
2016-09-28 15:55:08 -0600

    add song button

--- src/server/routes/signup.js
+++ src/server/routes/signup.js
@@ -11,31 +11,31 @@ router.get('/', function (req, res, next) {
     res.render('signup', renderObject);
   });
 
-  router.post('/', function (req, res, next) {
-    console.log(req.body);
-    const first_name = req.body.first_name;
-    const last_name = req.body.last_name;
-    const email = req.body.email;
-    const userName = req.body.userName;
-    const password = req.body.password;
-    let hash = bcrypt.hashSync(password, salt);
-    console.log(hash);
+router.post('/', function (req, res, next) {
+  console.log(req.body);
+  const first_name = req.body.first_name;
+  const last_name = req.body.last_name;
+  const email = req.body.email;
+  const userName = req.body.userName;
+  const password = req.body.password;
+  let hash = bcrypt.hashSync(password, salt);
+  console.log(hash);
 
-    knex('users')
-    .insert({
-      first_name:first_name,
-      last_name: last_name,
-      email: email,
-      userName: userName,
-      password: hash})
-    .then((newUser) => {
+  knex('users')
+  .insert({
+    first_name:first_name,
+    last_name: last_name,
+    email: email,
+    userName: userName,
+    password: hash})
+  .then((newUser) => {
 
-      res.send(newUser);
-    })
-    .catch((err) => {
-      res.send('Error');
-    });
+    res.send(newUser);
+  })
+  .catch((err) => {
+    res.send('Error');
   });
+});
 
 
 module.exports = router;
