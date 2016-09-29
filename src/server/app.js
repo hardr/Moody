49bb9d46d401dd2c4f05c1d0d8aeb45303bd40e9
Derek Styer <dmstyer@gmail.com>
2016-09-29 12:44:32 -0600

    almost done with the value chain

--- src/server/app.js
+++ src/server/app.js
@@ -4,18 +4,19 @@
 
   // *** dependencies *** //
   const express = require('express');
-
+  const app = express();
   const appConfig = require('./config/main-config.js');
   const routeConfig = require('./config/route-config.js');
   const errorConfig = require('./config/error-config.js');
+  const http = require('http').Server(app);
 
-  // *** express instance *** //
-  const app = express();
+
 
   // *** config *** //
   appConfig.init(app, express);
   routeConfig.init(app);
   errorConfig.init(app);
+
 
   module.exports = app;
 
